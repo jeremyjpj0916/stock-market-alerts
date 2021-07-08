75bd59028aa07fa0cb8949a010e71d9817fb13dc
Jeremy Justus <jeremyjustus0916@gmail.com>
2021-07-08 04:30:57 -0400

    Default sms alerts via gmail sender

--- main.py
+++ main.py
@@ -1,143 +1,62 @@
 import csv
 import io
-
-import pysbr
-import requests
+import yahooquery
 import classes
-from pysbr import *
 from datetime import datetime
 import time
+import asyncio
+import re
+from email.message import EmailMessage
+from typing import Tuple, Union
+import aiosmtplib
+
+# Email script from https://github.com/acamso/demos/blob/master/_email/send_txt_msg.py
+
+HOST = "smtp.gmail.com"
+# https://kb.sandisk.com/app/answers/detail/a_id/17056/~/list-of-mobile-carrier-gateway-addresses
+# https://www.gmass.co/blog/send-text-from-gmail/
+CARRIER_MAP = {
+    "verizon": "vtext.com",
+    "tmobile": "tmomail.net",
+    "sprint": "messaging.sprintpcs.com",
+    "at&t": "txt.att.net",
+    "boost": "smsmyboostmobile.com",
+    "cricket": "sms.cricketwireless.net",
+    "uscellular": "email.uscc.net",
+}
 
 
-def process_betting_game(game, event_list, filtered_games_list):
-    print(game.league + " LEAGUE GAME PROCESSING")
-    for event_match in event_list:
-        team_name = ""
-        if isinstance(event_match['participant full name'], str):
-            # Default as well as NBA for now:
-            team_name = event_match['participant full name'].split(" ")[1:][
-                0].lower()  # Drop the leading city to match analytics site name
-            if game.league == "NBA":
-                team_name = event_match['participant full name'].split(" ")[1:][
-                    0].lower()  # Drop the leading city to match analytics site name
-            if game.league == "EPL":
-                team_name = event_match['participant full name'].split(" ")[0:][
-                    0].lower()  # Drop the trailing info just take first word (city)
+# pylint: disable=too-many-arguments
+async def send_txt(
+    num: Union[str, int], carrier: str, email: str, pword: str, msg: str, subj: str
+) -> Tuple[dict, str]:
+    to_email = CARRIER_MAP[carrier]
 
-        if team_name == game.team_chosen.lower():  # match found
-            if event_match['american odds'] >= int(game.american_odds):
-                if isinstance(game.bookie_odds, int):  # If another bookie has better value replace again
-                    if event_match['american odds'] > game.bookie_odds:
-                        game.bookie = event_match['sportsbook']
-                        game.bookie_odds = event_match['american odds']
-                        filtered_games_list = filtered_games_list[:-1]  # Remove last appended odds for best odds
-                        filtered_games_list.append(game)
-                else:
-                    game.bookie = event_match['sportsbook']
-                    game.bookie_odds = event_match['american odds']
-                    filtered_games_list.append(game)
+    # build message
+    message = EmailMessage()
+    message["From"] = email
+    message["To"] = f"{num}@{to_email}"
+    message["Subject"] = subj
+    message.set_content(msg)
 
+    # send
+    send_kws = dict(username=email, password=pword, hostname=HOST, port=587, start_tls=True)
+    res = await aiosmtplib.send(message, **send_kws)  # type: ignore
+    msg = "failed" if not re.search(r"\sOK\s", res[1]) else "succeeded"
+    print(msg)
+    return res
 
 if __name__ == '__main__':
 
-    # Find leagues test
-    # league = pysbr.SearchLeagues("Turkish")
 
-    ### First portion, scrape all games we want to bet on ###
 
-    url = 'https://raw.githubusercontent.com/axz2000/sportsbook/main/masterScript/masterPush.csv'
-    response = requests.get(url)
-    csv_bytes = response.content
-    str_file = io.StringIO(csv_bytes.decode('utf-8'), newline='\n')
-    csvReader = csv.reader(str_file)
 
-    header = next(csvReader)
-    teamChosenIndex = header.index("Bet State Chosen")
-    percentageIndex = header.index("Allocation Percentage")
-    leagueIndex = header.index("League")
-    per_dollar_payoutIndex = header.index("Payouts (per Dollar)")
-    dateIndex = header.index("Date")
-    american_oddsIndex = header.index("American Odds")
-    # Make an empty games list
-    gamesList = []
-
-    # Loop through the lines in the file and get each element we want for sportsbetgame object and add it to the list of games.
-    for row in csvReader:
-        tempGame = classes.SportsBetGame(row[teamChosenIndex], row[percentageIndex], row[leagueIndex],
-                                         row[per_dollar_payoutIndex], row[dateIndex], row[american_oddsIndex], "", "")
-        gamesList.append(tempGame)
-
-    ### Second portion, check if games we want to bet on exist at favorable odds with bookies ###
-
-    ### Configurable variables ###
-
-    # Example options found here: https://github.com/JeMorriso/PySBR/blob/main/pysbr/config/sportsbooks.yaml
-    bookie_list = ['BetOnline', 'Bovada']
-    game_type = ['moneyline']
-
-    ### END OF Configurable variables ###
-
-    dt = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d')
-    nfl = NFL()
-    nba = NBA()
-    epl = EPL()
-    npl = Eredivisie()
-    sb = Sportsbook()
-    # Sleep for two seconds between various API queries to try to avoid hard traffic hitting
-    delay = 2
-    e_nfl = EventsByDate(nfl.league_id, dt)
-    time.sleep(delay)
-    e_nba = EventsByDate(nba.league_id, dt)
-    time.sleep(delay)
-    e_epl = EventsByDate(epl.league_id, dt)
-    time.sleep(delay)
-    e_npl = EventsByDate(npl.league_id, dt)
-    time.sleep(delay)
-    cl_nfl = CurrentLines(e_nfl.ids(), nfl.market_ids(game_type), sb.ids(bookie_list))
-    time.sleep(delay)
-    cl_nba = CurrentLines(e_nba.ids(), nba.market_ids(game_type), sb.ids(bookie_list))
-    time.sleep(delay)
-    cl_epl = CurrentLines(e_epl.ids(), epl.market_ids(game_type), sb.ids(bookie_list))
-    time.sleep(delay)
-    cl_npl = CurrentLines(e_npl.ids(), npl.market_ids(game_type), sb.ids(bookie_list))
-
-    # List to contain matches that meet our bookie criteria.
-    filtered_games_list = []
-
-    # NOTE, need to adjust search for the participant full name vs event to get right odds
-
-    for game in gamesList:
-        if game.team_chosen.startswith('Draw '):  # Ignore "Draw" picks for now, unsure how to handle.
-            continue
-
-        if game.league == "KHL":
-            print(game)
-        elif game.league == "NHL":
-            print(game)
-        elif game.league == "NFL":
-            process_betting_game(game, cl_nfl.list(e_nfl), filtered_games_list)
-        elif game.league == "NBA":
-            process_betting_game(game, cl_nba.list(e_nba), filtered_games_list)
-        elif game.league == "LLA":
-            print(game)
-        elif game.league == "PPL":
-            print(game)
-        elif game.league == "ELO":
-            print(game)
-        elif game.league == "EPL":
-            process_betting_game(game, cl_epl.list(e_epl), filtered_games_list)
-        elif game.league == "GPL":
-            print(game)
-        elif game.league == "ISA":
-            print(game)
-        elif game.league == "TSL":
-            print(game)
-        elif game.league == "NPL":
-            process_betting_game(game, cl_npl.list(e_npl), filtered_games_list)
-        elif game.league == "FL1":
-            print(game)
-        elif game.league == "BPL":
-            print(game)
-
-    # Print the games list as a test.
-    print(filtered_games_list)
+    #Alert sending portion/config
+    _num = "XXXXXXXXXX"
+    _carrier = "verizon"
+    _email = "XXXXXXXX@gmail.com"
+    _pword = "XXXXXXXX"
+    _msg = "Dummy stock msg 3"
+    _subj = "Dummy stock subj 3"
+    coro = send_txt(_num, _carrier, _email, _pword, _msg, _subj)
+    asyncio.run(coro)
