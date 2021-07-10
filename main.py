f3462eea5bc111cdcdc836f3b6e1cefb32a3c23f
Jeremy Justus <jeremyjustus0916@gmail.com>
2021-07-10 03:25:22 -0400

    Enhanced to allow multiple phone numbers list to send the alerts to from single running app. Avoid weekend polling when markets closed. Add % change rounded to the texts too vs just only the price.

--- main.py
+++ main.py
@@ -26,13 +26,16 @@ CARRIER_MAP = {
 
 def send_message(_subj, _msg):
     # Alert sending portion config
-    _num = "XXXXXXXXXX"
+    _num_list = ["XXXXXXXXXX"]
     _carrier = "verizon"
-    _email = "XXXXXXX@gmail.com"
-    _password = "XXXXXXX"
+    _email = "XXXXXXXX@gmail.com"
+    _password = "XXXXXXXX"
     # Alert SMS
-    sms = send_txt(_num, _carrier, _email, _password, _msg, _subj)
-    asyncio.run(sms)
+    for _num in _num_list:
+        coro = send_txt(_num, _carrier, _email, _password, _msg, _subj)
+        asyncio.run(coro)
+        # Sleep 3 seconds between texts to diff numbers(unsure googles rate limit or protections for rapid texts).
+        time.sleep(3)
 
 
 # pylint: disable=too-many-arguments
@@ -76,33 +79,33 @@ def evaluate_watch_list(watch_list, market_phase, change_percent_threshold):
                 pre_market_change_percent = yahoo_stock_price_details[company_stock]["preMarketChangePercent"]
                 if pre_market_change_percent >= change_percent_threshold:
                     send_message(company_stock + " ALERT UP", company_stock + " Price: " + str(
-                        yahoo_stock_price_details[company_stock]["preMarketPrice"]))
+                        yahoo_stock_price_details[company_stock]["preMarketPrice"]) + ", Change: " + str((100*round(pre_market_change_percent, 4))) + "%")
                     company.alerted_already = True
                 elif pre_market_change_percent <= -change_percent_threshold:
                     send_message(company_stock + " ALERT DOWN", company_stock + " Price: " + str(
-                        yahoo_stock_price_details[company_stock]["preMarketPrice"]))
+                        yahoo_stock_price_details[company_stock]["preMarketPrice"]) + ", Change: " + str((100*round(pre_market_change_percent, 4))) + "%")
                     company.alerted_already = True
             elif market_phase == "regular" and "regularMarketChangePercent" in yahoo_stock_price_details[company_stock]:
                 # Ex: -0.0012641911 / 0.0012641911
                 regular_market_change_percent = yahoo_stock_price_details[company_stock]["regularMarketChangePercent"]
                 if regular_market_change_percent >= change_percent_threshold:
                     send_message(company_stock + " ALERT UP", company_stock + " Price: " + str(
-                        yahoo_stock_price_details[company_stock]["regularMarketPrice"]))
+                        yahoo_stock_price_details[company_stock]["regularMarketPrice"]) + ", Change: " + str((100*round(regular_market_change_percent, 4))) + "%")
                     company.alerted_already = True
                 elif regular_market_change_percent <= -change_percent_threshold:
                     send_message(company_stock + " ALERT DOWN", company_stock + " Price: " + str(
-                        yahoo_stock_price_details[company_stock]["regularMarketPrice"]))
+                        yahoo_stock_price_details[company_stock]["regularMarketPrice"]) + ", Change: " + str((100*round(regular_market_change_percent, 4))) + "%")
                     company.alerted_already = True
             elif market_phase == "post" and "postMarketChangePercent" in yahoo_stock_price_details[company_stock]:
                 # Ex: -0.0012641911 / 0.0012641911
                 post_market_change_percent = yahoo_stock_price_details[company_stock]["postMarketChangePercent"]
                 if post_market_change_percent >= change_percent_threshold:
                     send_message(company_stock + " ALERT UP", company_stock + " Price: " + str(
-                        yahoo_stock_price_details[company_stock]["postMarketPrice"]))
+                        yahoo_stock_price_details[company_stock]["postMarketPrice"]) + ", Change: " + str((100*round(post_market_change_percent, 4))) + "%")
                     company.alerted_already = True
                 elif post_market_change_percent <= -change_percent_threshold:
                     send_message(company_stock + " ALERT DOWN", company_stock + " Price: " + str(
-                        yahoo_stock_price_details[company_stock]["postMarketPrice"]))
+                        yahoo_stock_price_details[company_stock]["postMarketPrice"]) + ", Change: " + str((100*round(post_market_change_percent, 4))) + "%")
                     company.alerted_already = True
 
 
@@ -134,10 +137,14 @@ if __name__ == '__main__':
     while True:
         # est datetime
         est_dt = datetime.now(eastern).time()
+        weekday = datetime.now(eastern).today().weekday()  # 5 Monday is 0 and Sunday is 6.
 
         # Do nothing from 8pm EST to 4am EST.
-        if is_now_in_time_period(dt.time(20, 00), dt.time(4, 00), est_dt):
-            print("**Do nothing between 8PM EST and 4AM EST market hours**")
+        if is_now_in_time_period(dt.time(20, 00), dt.time(4, 00), est_dt) or (weekday >= 5):
+            if weekday >= 5:
+                print("**Do nothing on closed market days saturday and sunday weekend**")
+            else:
+                print("**Do nothing between 8PM EST and 4AM EST market hours**")
             time.sleep(60)  # Sleep 1 minute checks when in the quiet period
             continue
         elif is_now_in_time_period(dt.time(4, 00), dt.time(9, 30), est_dt):  # Premarket analysis
